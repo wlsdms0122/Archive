90a9aa98f55cc0e1b067c167a0b252b65c65fe8c
Jeong Jineun <wlsdms0122@gmail.com>
2023-02-24 15:07:19 +0900

    feat(): update `ArrayBuilder`

--- Sources/Archive/Foundation/ArrayBuilder.swift
+++ Sources/Archive/Foundation/ArrayBuilder.swift
@@ -8,8 +8,44 @@
 import Foundation
 
 @resultBuilder
-struct ArrayBuilder<Element> {
-    static func buildBlock(_ components: Element...) -> [Element] {
-        components
+public struct ArrayBuilder<Element> {
+    public static func buildBlock(_ components: Element...) -> [Element] {
+        return components
+    }
+    
+    public static func buildArray(_ components: [[Element]]) -> [Element] {
+        return components.flatMap { $0 }
+    }
+    
+    public static func buildOptional(_ component: [Element]?) -> [Element] {
+        return component ?? []
+    }
+    
+    public static func buildEither(first component: [Element]) -> [Element] {
+        return component
+    }
+    
+    public static func buildEither(second component: [Element]) -> [Element] {
+        return component
+    }
+    
+    public static func buildIf(_ element: [Element]?) -> [Element] {
+        element ?? []
+    }
+    
+    public static func buildPartialBlock(first: Element) -> [Element] {
+        [first]
+    }
+    
+    public static func buildPartialBlock(first: [Element]) -> [Element] {
+        first
+    }
+    
+    public static func buildPartialBlock(accumulated: [Element], next: Element) -> [Element] {
+        accumulated + [next]
+    }
+    
+    public static func buildPartialBlock(accumulated: [Element], next: [Element]) -> [Element] {
+        accumulated + next
     }
 }
