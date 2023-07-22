66159a9255747d92f0ca559526f26d754061305d
Jeong Jineun <wlsdms0122@gmail.com>
2023-07-23 01:12:50 +0900

    refac(): remove omissible `return`

--- Sources/Archive/UIKit/Extension/UIColor+Trait.swift
+++ Sources/Archive/UIKit/Extension/UIColor+Trait.swift
@@ -9,7 +9,7 @@ import UIKit
 
 public extension UIColor {
     static func traitColor(_ light: UIColor, dark: UIColor? = nil) -> UIColor {
-        return .init { collection in
+        .init { collection in
             switch collection.userInterfaceStyle {
             case .dark:
                 return dark ?? light
